0c8ddda81c11eeec5782b8a82187710dccd9f24e
trillemax <angelo.gallego@udea.edu.co>
2026-06-02 16:02:45 -0500

    fix cors railway

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,19 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
-app.options('*', cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://lol-stats-x3ln.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
